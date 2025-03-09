ecb60281e40dfd1748ba84f836402bfcfa059e69
Harshith Bathula <37700453+HarshithBathula@users.noreply.github.com>
2025-03-08 18:30:19 -0800

    Time: 3 ms (63.48%) | Memory: 13.9 MB (55.17%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -5,14 +5,9 @@ class Solution(object):
         :rtype: int
         """
         j = 0
-        unique = True
 
         for i in range(1,len(nums)):
             if nums[i] > nums[i-1]:
-                unique = True;
-            elif nums[i] == nums[i-1]:
-                unique = False;
-            if unique:
                 nums[j+1] = nums[i];
                 j = j+1;
         return j+1        
